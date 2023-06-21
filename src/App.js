dd082a2c1133d8a29c3c68dd7ecc598a0868d034
rushibelkunde <rushibelkunde18@gmail.com>
2023-06-21 19:58:10 +0530

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,108 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState, useReducer, useRef } from "react";
+import ExpenseForm from "./components/ExpenseForm/ExpenseForm";
+import ExpenseInfo from "./components/ExpenseInfo/ExpenseInfo";
+import ExpenseList from "./components/ExpenseList/ExpenseList";
+import "./App.css";
+
+const reducer = (state, action) => {
+  const { payload } = action;
+  switch (action.type) {
+    case "ADD_EXPENSE": {
+      return {
+        expenses: [payload.expense, ...state.expenses]
+      };
+    }
+    case "REMOVE_EXPENSE": {
+      return {
+        expenses: state.expenses.filter((expense) => expense.id !== payload.id)
+      };
+    }
+
+    case "UPDATE_EXPENSE":{
+      let index = -1
+      let array = [...state.expenses]
+      array.forEach((item,i)=>{
+        if(item.id==payload.editID){
+          index = i
+        }
+      })
+      array[index] = payload.expense
+      return {
+        expenses: array
+      };
+    }
+    default:
+      return state;
+  }
+};
+
+
 
 function App() {
+  const expenseTextInput = useRef()
+  const expenseAmountInput = useRef()
+  const [state, dispatch] = useReducer(reducer, { expenses: [] });
+  const [toUpdate , setUpdate] = useState(false)
+  const [editID , setID] = useState("")
+  
+
+  const addExpense = (expense) => {
+    dispatch({ type: "ADD_EXPENSE", payload: { expense } });
+  };
+
+  const deleteExpense = (id) => {
+    dispatch({ type: "REMOVE_EXPENSE", payload: { id } });
+  };
+
+
+  const changeExpenseToUpdate = (id) => { 
+    let index = -1
+    state.expenses.forEach((item,i)=>{
+      if(item.id==id){
+        index = i
+      }
+    })
+    expenseTextInput.current.value = state.expenses[index].text
+    expenseAmountInput.current.value =  state.expenses[index].amount
+    setUpdate(true)
+    setID(id)
+
+    // dispatch({ type: "UPDATE_EXPENSE", payload: { id } });
+  }
+
+  const editExpense = (expense)=>{
+
+    dispatch({ type: "UPDATE_EXPENSE", payload: { expense, editID: expense.id } });
+    setUpdate(false)
+
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <h2 className="mainHeading">Expense Tracker</h2>
+      <div className="App">
+      <ExpenseForm
+        expenseTextInput={expenseTextInput}
+        expenseAmountInput={expenseAmountInput}
+         addExpense={addExpense}
+         toUpdate={toUpdate}
+         editExpense= {editExpense}
+         editID={editID} />
+      
+        <div className="expenseContainer">
+        
+        
+          <ExpenseInfo expenses={state.expenses} />
+          <ExpenseList
+            expenses={state.expenses}
+            deleteExpense={deleteExpense}
+            toUpdate={toUpdate}
+            changeExpenseToUpdate = {changeExpenseToUpdate}
+            
+          />
+        </div>
+      </div>
+    </>
   );
 }
 
---
+++ src/components/ExpenseForm/ExpenseForm.js
@@ -0,0 +1,94 @@
+
+import styles from "./ExpenseForm.module.css";
+
+const ExpenseForm = ({ addExpense, toUpdate , editExpense, editID ,expenseTextInput, expenseAmountInput}) => {
+  
+  
+  // Use the useEffect hook here, to check if an expense is to be updated
+  // If yes, the autofill the form values with the text and amount of the expense
+  
+
+  const onSubmitHandler = (e) => {
+    e.preventDefault();
+    let expenseText = expenseTextInput.current.value;
+    let expenseAmount = expenseAmountInput.current.value;
+    if (parseInt(expenseAmount) === 0) {
+      return;
+    }
+
+
+    if(toUpdate){
+      const expense = {
+        text: expenseText,
+        amount: expenseAmount,
+        id: editID,
+      };
+
+      editExpense(expense)
+
+      clearInput();
+      return;
+
+    }
+    else{
+      const expense = {
+        text: expenseText,
+        amount: expenseAmount,
+        id: new Date().getTime(),
+      };
+      addExpense(expense);
+      clearInput();
+      return;
+    }
+
+    
+
+    // Logic to update expense here
+  };
+
+  const clearInput = () => {
+    expenseAmountInput.current.value = "";
+    expenseTextInput.current.value = "";
+  };
+
+  
+   
+
+  
+
+  
+
+  return (
+    <form className={styles.form} onSubmit={onSubmitHandler}>
+      {/* Change text to Edit Transaction if an expense is to be updated */}
+      <h3>{toUpdate? "Edit Transaction" : "Add new transaction"}</h3>
+      <label htmlFor="expenseText">Text</label>
+      <input
+        id="expenseText"
+        className={styles.input}
+        type="text"
+        placeholder="Enter text..."
+        ref={expenseTextInput}
+        required
+      />
+      <div>
+        <label htmlFor="expenseAmount">Amount</label>
+        <div>(negative - expense,positive-income)</div>
+      </div>
+      <input
+        className={styles.input}
+        id="expenseAmount"
+        type="number"
+        placeholder="Enter amount..."
+        ref={expenseAmountInput}
+        required
+      />
+      <button className={styles.submitBtn}>
+        {toUpdate? "Edit Transaction" : "Add Transaction"}
+      </button>
+    </form>
+  );
+};
+
+
+export default ExpenseForm;
